c96a4cad2e3a155bb152c7f705a35ff6376c055d
kim <s158614@nishitech.ac.jp>
2020-02-01 11:13:17 +0900

    Custom Icons for the Tab Navigation

--- navigation/TabNavigation.js
+++ navigation/TabNavigation.js
@@ -1,20 +1,67 @@
+import React from "react";
+import {Platform} from "react-native";
+
 import {createAppContainer} from "react-navigation";
 import {createBottomTabNavigator} from "react-navigation-tabs"
 
 import MoviesScreen from "../screens/Movies";
 import TVScreen from "../screens/TV";
 import SearchScreen from "../screens/Search";
+import DetailScreen from "../screens/Detail";
 import {BG_COLOR} from "../constants/Colors";
+
+import TabBarIcon from "../components/TabBarIcon";
 
 const TabNavigation = createBottomTabNavigator(
     {
-        Movies: MoviesScreen,
-        TV: TVScreen,
-        Search: SearchScreen
+        Movie: {
+            screen: MoviesScreen,
+            navigationOptions: {
+                tabBarIcon: ({focused}) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-film" : "md-film"}
+                    />
+                )
+            }
+        },
+        TV: {
+            screen: TVScreen,
+            navigationOptions: {
+                tabBarIcon: ({focused}) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-tv" : "md-tv"}
+                    />
+                )
+            }
+        },
+        Search: {
+            screen:SearchScreen,
+            navigationOptions: {
+                tabBarIcon: ({focused}) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-search" : "md-search"}
+                    />
+                )
+            }
+        },
+        Detail: {
+            screen: DetailScreen,
+            navigationOptions: {
+                tabBarIcon: ({focused}) => (
+                    <TabBarIcon
+                        focused={focused}
+                        name={Platform.OS === "ios" ? "ios-settings" : "md-settings"}
+                    />
+                )
+            }
+        }
     },
     {
         tabBarOptions: {
-            showLabel: true,
+            showLabel: false,
             style: {
                 backgroundColor: BG_COLOR
             }
